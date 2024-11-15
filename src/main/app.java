d1803a1190561313b8db7f2d8560c2eed5854844
Mujtaba <ma.mujtaba12@gmail.com>
2024-11-15 11:01:00 +0500

    necessary change

--- src/main/app.java
+++ src/main/app.java
@@ -1,12 +1,12 @@
 package main;
 
 public class app {
-public static void main(String[] args) {
-System.out.println("Welcome to the application!");
-greetUser();
-}
+	public static void main(String[] args) {
+		System.out.println("Welcome to the application!");
+		greetUser();
+	}
 
-public static void greetUser() {
-System.out.println("Hello, User!");
-}
+	public static void greetUser() {
+		System.out.println("Hello, User!");
+	}
 }
