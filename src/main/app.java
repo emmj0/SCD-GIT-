af5ea72e6be7329d920f90f73c900b0777ba0848
unknown <digitalsarmad007@gmail.com>
2024-11-15 10:39:54 +0500

    added bug

--- src/main/app.java
+++ src/main/app.java
@@ -3,7 +3,7 @@ package main;
 public class app {
 public static void main(String[] args) {
 System.out.println("Welcome to the application!");
-greetUser();
+//greetUser();
 }
 
 public static void greetUser() {
