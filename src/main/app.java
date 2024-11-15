0d683876a98099c198e46ca2bcf0fcd7e332505f
unknown <digitalsarmad007@gmail.com>
2024-11-15 11:06:11 +0500

    main branch update

--- src/main/app.java
+++ src/main/app.java
@@ -3,6 +3,7 @@ package main;
 public class app {
 public static void main(String[] args) {
 System.out.println("Welcome to the application!");
+System.out.println("Main Branch Updated");
 greetUser();
 }
 
